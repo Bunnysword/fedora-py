3115c506bd7e64e68693fc5fb4dd42af7611b62a
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2024-08-22 09:04:17 +0300

    Rename fpk.sh to scripts/fpk.sh

--- fpk.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-#fpk - flatpaks apps
-flatpak install flathub -y com.obsproject.Studio portproton net.davidotek.pupgui2
---
+++ scripts/fpk.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+#fpk - flatpaks apps
+flatpak install flathub -y com.obsproject.Studio portproton net.davidotek.pupgui2
