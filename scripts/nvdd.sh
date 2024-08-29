43171f7c1d2aff919af1273b545498e9e2eea856
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2024-08-29 08:51:43 +0300

    Update nvdd.sh

--- scripts/nvdd.sh
+++ scripts/nvdd.sh
@@ -7,7 +7,7 @@ sudo dnf copr enable codifryed/CoolerControl
 sudo dnf install coolercontrol
 sudo systemctl enable --now coolercoorg.fredesktop.Pla
 #560.28.03
-sudo dnf --enablerepo=rpmfusion-nonfree-updates-testing install akmod-nvidia xorg-x11-drv-nvidia-libs.i686
+sudo dnf --enablerepo=rpmfusion-nonfree-updates-testing install akmod-nvidia-open xorg-x11-drv-nvidia-libs.i686 xorg-x11-drv-nvidia-cuda-libs.i686
 #
 flatpak install -y org.freedesktop.Platform.GL.nvidia-555-58-02
 ####nvidia-kms
